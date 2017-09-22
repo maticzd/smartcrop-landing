f937138c3c116d7a3a3fa13afef41c5a18f6b120
Rodrigo Lagos <rodrigo.lagos.13@sansano.usm.cl>
2017-09-22 16:42:30 -0300

    Fix some js and css rules

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
+/* global jQuery, $, SmoothScroll */
+
 /* Sticky menu animations */
-jQuery(window).on("scroll", function () {
-    if (jQuery(this).scrollTop() > 1) {
-        jQuery(".navbar-fixed-top").addClass("navbar-scroll");
-    }
-    else {
-        jQuery(".navbar-fixed-top").removeClass("navbar-scroll");
-    }
-});
+jQuery(window).on('scroll', function () {
+  if (jQuery(this).scrollTop() > 1) {
+    jQuery('.navbar-fixed-top').addClass('navbar-scroll')
+  } else {
+    jQuery('.navbar-fixed-top').removeClass('navbar-scroll')
+  }
+})
 
 /* Smooth Scroll */
-var scroll = new SmoothScroll('a[href*="#"]', {
+var scroll = new SmoothScroll('a[href*="#"]', { // eslint-disable-line
   offset: '60px'
-});
+})
 
 /* Animsition configuration */
-$(document).ready(function() {
-  $(".animsition").animsition({
+$(document).ready(function () {
+  $('.animsition').animsition({
     inClass: 'fade-in',
     outClass: 'fade-out',
     inDuration: 1500,
@@ -23,18 +24,18 @@ $(document).ready(function() {
     linkElement: '.animsition-link',
     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
     loading: true,
-    loadingParentElement: 'html', //animsition wrapper element
+    loadingParentElement: 'html', // animsition wrapper element
     loadingClass: 'animsition-loading',
     loadingInner: '', // e.g '<img src="loading.svg" />'
     timeout: false,
     timeoutCountdown: 5000,
     onLoadEvent: true,
-    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    browser: ['animation-duration', '-webkit-animation-duration'],
     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay : false,
-    overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'body',
-    transition: function(url){ window.location.href = url; }
-  });
-});
+    overlay: false,
+    overlayClass: 'animsition-overlay-slide',
+    overlayParentElement: 'body',
+    transition: function (url) { window.location.href = url }
+  })
+})
