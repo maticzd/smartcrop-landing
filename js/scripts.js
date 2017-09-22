bd46913aecfc9048555f9105afa8a01238c3d5cc
Rodrigo Lagos <rodrigo.lagos.13@sansano.usm.cl>
2017-09-22 17:36:36 -0300

    Change animsition configuration

--- js/scripts.js
+++ js/scripts.js
@@ -16,24 +16,24 @@ var scroll = new SmoothScroll('a[href*="#"]', { // eslint-disable-line
 
 /* Animsition configuration */
 jQuery(".animsition").animsition({
-		inClass: 'fade-in',
-		outClass: 'fade-out',
-		inDuration: 800,
-		outDuration: 300,
-		linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([data-featherlight="image"])',
-		// e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-		loading: true,
-		loadingParentElement: 'html', //animsition wrapper element
-		loadingClass: 'animsition-loading',
-		loadingInner: '', // e.g '<img src="loading.svg" />'
-		timeout: false,
-		timeoutCountdown: 5000,
-		onLoadEvent: true,
-		browser: [ 'animation-duration', '-webkit-animation-duration'],
-		// "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-		// The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-		overlay : false,
-		overlayClass : 'animsition-overlay-slide',
-		overlayParentElement : 'body',
-		transition: function(url){ window.location.href = url; }
-	});
+	inClass: 'fade-in',
+	outClass: 'fade-out',
+	inDuration: 800,
+	outDuration: 300,
+	linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([data-featherlight="image"])',
+	// e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+	loading: true,
+	loadingParentElement: 'html', //animsition wrapper element
+	loadingClass: 'animsition-loading',
+	loadingInner: '', // e.g '<img src="loading.svg" />'
+	timeout: false,
+	timeoutCountdown: 5000,
+	onLoadEvent: true,
+	browser: [ 'animation-duration', '-webkit-animation-duration'],
+	// "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+	// The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+	overlay : false,
+	overlayClass : 'animsition-overlay-slide',
+	overlayParentElement : 'body',
+	transition: function(url){ window.location.href = url; }
+});
