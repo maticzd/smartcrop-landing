e6e695c3f8c5a28104aafedaf538a658c02061f1
Rodrigo Lagos <rodrigo.lagos.13@sansano.usm.cl>
2017-09-22 16:03:21 -0300

    Add animsitionjs

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,29 @@ jQuery(window).on("scroll", function () {
 var scroll = new SmoothScroll('a[href*="#"]', {
   offset: '60px'
 });
+
+/* Animsition configuration */
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'html', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+});
