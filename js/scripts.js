0256f7e6e541a4de38c7a99b9bb5766049336f08
Rodrigo Lagos <rodrigo.lagos.13@sansano.usm.cl>
2017-09-23 04:19:52 -0300

    Fix navbar problem in mobile

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,9 @@ jQuery(window).scroll(function () {
     jQuery('.navbar-fixed-top').addClass('navbar-scroll')
     jQuery('.navbar-fixed-top').css('background', 'rgba(58,96,27,.64)')
   } else if (jQuery(this).scrollTop() >= windowHeight) {
-    jQuery('.navbar-fixed-top').css('background', 'rgb(55,101,18)')
+    jQuery('.menu').addClass('menu-scroll')
     jQuery('.navbar-fixed-top').addClass('navbar-scroll')
+    jQuery('.navbar-fixed-top').css('background', 'rgb(55,101,18)')
   } else if (jQuery(this).scrollTop() <= 1) {
     jQuery('.menu').removeClass('menu-scroll')
     jQuery('.navbar-fixed-top').removeClass('navbar-scroll')
